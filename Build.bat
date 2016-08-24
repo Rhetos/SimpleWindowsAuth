94399aaaeffc993b2e23e2361b47afe4fbb1eee3
Bojan Antolović <bojan.antolovic@gmail.com>
2016-08-24 16:44:24 +0200

    Referencing Rhetos NuGet instead of a relative path.

---
+++ Build.bat
@@ -0,0 +1,30 @@
+@REM HINT: SET SECOND ARGUMENT TO /NOPAUSE WHEN AUTOMATING THE BUILD.
+
+CALL "%VS140COMNTOOLS%VsDevCmd.bat" || ECHO ERROR: Cannot find Visual Studio 2015, missing VS140COMNTOOLS variable. && GOTO Error0
+@ECHO ON
+
+PUSHD "%~dp0" || GOTO Error0
+CALL ChangeVersions.bat || GOTO Error1
+IF EXIST msbuild.log DEL msbuild.log || GOTO Error1
+
+REM NuGet Automatic Package Restore requires "NuGet.exe restore" to be executed before the command-line build.
+WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error1
+NuGet.exe restore Rhetos.SimpleWindowsAuth.sln -NonInteractive || GOTO Error1
+MSBuild.exe "Rhetos.SimpleWindowsAuth.sln" /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error1
+NuGet.exe pack -o .. || GOTO Error1
+CALL ChangeVersions.bat /RESTORE || GOTO Error1
+POPD
+
+@REM ================================================
+
+@ECHO.
+@ECHO %~nx0 SUCCESSFULLY COMPLETED.
+@EXIT /B 0
+
+:Error1
+@POPD
+:Error0
+@ECHO.
+@ECHO %~nx0 FAILED.
+@IF /I [%2] NEQ [/NOPAUSE] @PAUSE
+@EXIT /B 1
--- Plugins/Rhetos.Security.Service/Properties/AssemblyInfo.cs
+++ Plugins/Rhetos.Security.Service/Properties/AssemblyInfo.cs
@@ -48,3 +48,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyCompany("Omega software")]
+[assembly: AssemblyCopyright("Copyright (C) Omega software 2016")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-dev")]
